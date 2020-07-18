7e52d97c0c192caab361d40e1b7f54829899a0aa
dbuana <46995714+dbuana@users.noreply.github.com>
2020-07-18 19:30:55 +0900

    Basic C# Program

---
+++ basics.cs
@@ -0,0 +1,25 @@
+using System;
+
+// Collection of basic C# program; it includes "Hello, World!", variable types and basic functions
+namespace Basic_Programming() {
+	class Program {
+		static void Main(string[] args){
+			// The most basic C# line written
+			Console.WriteLine("Hello, World!");
+			// Types of variables
+			int a = 1;
+			string b = "Hey!";
+			float c = 3.2;
+			char d = 2;
+			bool e = True;
+		}
+		// Basic function
+		void Firstfunction(){
+			return Console.WriteLine("Hey, Again!");
+		}
+		// Interestingly it can also be typed with function types
+		bool validation(a>5){
+			return Console.WriteLine(a>5);
+		}
+	}
+}
